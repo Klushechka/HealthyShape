8169e7635831d6fcbca8e8c8e1ac374463a8fd3f
Olga Klyushkina <klyushkina.olga@gmail.com>
2017-05-17 14:36:45 +0300

    Several changes have been implemented:
     - Segmented control for Man/Woman gender switching;
     - Resume is now different for BMI depending on whether gender is Man or Woman;
     - Rounding of BMI value to leave 1 figure after separator.

--- Healthy Shape/ViewController.swift
+++ Healthy Shape/ViewController.swift
@@ -15,12 +15,15 @@ class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet var getWeight: UITextField!
     @IBOutlet var getHeight: UITextField!
+    @IBOutlet weak var genderSwitcher: UISegmentedControl!
+    
     
     @IBAction func calculate(_ sender: Any) {
     }
     
     var myBMI: Float = 0.0
     var resume: String = ""
+    var gender: UInt8 = 2 // default value which is Man
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -56,18 +59,44 @@ class ViewController: UIViewController, UITextFieldDelegate {
         }
         
         func getResume(myBMI: Float) {
-            switch(myBMI) {
-            case 0:
-                resume = " . Please set weight and height values."
-            case 1..<18.5:
-                resume = ". You are underweighted."
-            case 18.5...24.9:
-                resume = ". You have a healthy weight."
-            case 25.0...29.9:
-                resume = ". You are overweighted."
-            case 30.0...9999999999999999999999999999:
-                resume = ". You have an obese."
-            default: break
+            if gender == 2 { // When gender is Man
+                switch(myBMI) {
+                    case 0:
+                        resume = " . Please set weight and height values."
+                    case 1.0..<16.0:
+                            resume = ". You are dangerously underweighted."
+                    case 16..<18.5:
+                            resume = ". You are underweighted."
+                    case 18.5..<25.0:
+                            resume = ". You have a healthy weight."
+                    case 25.0..<30:
+                            resume = ". You are overweighted."
+                    case 30.0..<35.0:
+                            resume = ". You have an obese (Class I)."
+                    case 35.0..<40.0:
+                            resume = ". You have an obese (Class II)."
+                    case 40.0...9999999999999999999999999999:
+                            resume = ". You have a morbid obese (Class III)."
+                    default: break
+                }
+            } else { // When gender is Woman
+                    switch(myBMI) {
+                    case 0:
+                        resume = " . Please set weight and height values."
+                    case 1.0..<16.0:
+                        resume = ". You are dangerously underweighted."
+                    case 16.0..<18.0:
+                        resume = ". You are underweighted."
+                    case 18.0..<25.0:
+                        resume = ". You have a healthy weight."
+                    case 25.0..<30:
+                        resume = ". You have an obese (Class I)."
+                    case 30.0..<40.0:
+                        resume = ". You have an obese (Class II)."
+                    case 40.0...9999999999999999999999999999:
+                        resume = ". You have a morbid obese (Class III)."
+                    default: break
+                }
             }
         }
     
@@ -77,7 +106,17 @@ class ViewController: UIViewController, UITextFieldDelegate {
         countBMI(userWeight: weight, userHeight: height)
         getResume(myBMI: myBMI)
         
-        DestViewController.BodyMassText = "Your BodyMassIndex is " + String(myBMI) + resume
+        DestViewController.BodyMassText = "Your BodyMassIndex is " + String(format: "%.1f", myBMI) + resume
+    }
+    
+    @IBAction func genderSwitchButton(_ sender: UISegmentedControl) {
+        switch genderSwitcher.selectedSegmentIndex {
+        case 0:
+            gender = 2 // this is Man
+        case 1:
+            gender = 3 // this is Woman
+        default: break
+        }
     }
 }
 
